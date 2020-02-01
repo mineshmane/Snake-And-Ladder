e5a8915c255baa79d6ee7c0501c31f39b69bf9ac
mineshmane <mineshmane94@gmail.com>
2020-02-01 15:23:10 +0530

    Added:UC3 check option done

--- shell.sh
+++ shell.sh
@@ -4,6 +4,18 @@ echo "welcome to Snake and Ladder game"
 
 INITIAL_POSITION=0;
 NUMBEROFPLAYER=1;
-
+firstPlayerPosition=$INITIAL_POSITION;
+NOPLAY=0;
+LADDER=1;
+SNAKE=2; 
 randomDieMove=$((RANDOM%6+1))
+Option=$((RANDOM%3))
 
+	case $Option in
+	$NOPLAY)
+		firstPlayerPosition=$firstPlayerPosition;;
+	$LADDER)
+		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ));;
+	$SNAKE)
+		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ));;
+	esac
