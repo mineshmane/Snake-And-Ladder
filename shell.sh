f5fe2bac62f4c6d7967ffa5dcdf9916ba703c8d3
mineshmane <mineshmane94@gmail.com>
2020-02-01 18:00:34 +0530

    Added:Win position is exacly 100 done

--- shell.sh
+++ shell.sh
@@ -1,39 +1,58 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 echo "welcome to Snake and Ladder game"
 
 INITIAL_POSITION=0;
-WINNING_POSITION=100;
+WINNING_POSITION=20;
 NUMBEROFPLAYER=1;
 firstPlayerPosition=$INITIAL_POSITION;
 NOPLAY=0;
 LADDER=1;
 SNAKE=2; 
 
+
 function moveCondition(){
 
 	if [ $1 -le 0 ]
 	then
-		echo $1=$INITIAL_POSITION;
-	elif [ $1 -ge $WINNING_POSITION ]
+		echo firstPlayerPosition=$INITIAL_POSITION;
+	elif [ $1 -eq $WINNING_POSITION ]
 	then
-		echo win;
+		echo "win" $1 $WINNING_POSITION
+	elif [ $1 -gt $WINNING_POSITION ]
+	then
+		firstPlayerPosition=$(( $1 - $2 ))
+		echo " greater than Wiining position " $firstPlayerPosition
 	fi 
 }
 
-while [ $firstPlayerPosition -le $WINNING_POSITION ]
+
+while [ $firstPlayerPosition -lt $WINNING_POSITION ]
 do
 	randomDieMove=$((RANDOM%6+1))
 	Option=$((RANDOM%3))
-
 	case $Option in
 	$NOPLAY)
-		firstPlayerPosition=$firstPlayerPosition;;
+		echo " random no play  move " $randomDieMove
+		echo " no play" $firstPlayerPosition
+		firstPlayerPosition=$firstPlayerPosition
+		;;
+
 	$LADDER)
-		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ));;
+		echo " ladder move " $randomDieMove
+		echo "Ladder" $firstPlayerPosition
+		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ))
+		;;
 	$SNAKE)
-		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ));;
+	echo "random snake move" $randomDieMove
+		echo "Anake" $firstPlayerPosition
+		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ))
+		;;
+
 	esac
-	 moveCondition $firstPlayerPosition
+	 moveCondition $firstPlayerPosition $randomDieMove
 
 done
+
+
+
