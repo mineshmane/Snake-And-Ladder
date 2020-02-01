13e78f0bce420103b506a94b1f1843d3964afbd2
mineshmane <mineshmane94@gmail.com>
2020-02-01 15:47:28 +0530

    Added:Repeat till player reach at wiining position 100

--- shell.sh
+++ shell.sh
@@ -1,15 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "welcome to Snake and Ladder game"
 
 INITIAL_POSITION=0;
+WINNING_POSITION=100;
 NUMBEROFPLAYER=1;
 firstPlayerPosition=$INITIAL_POSITION;
 NOPLAY=0;
 LADDER=1;
 SNAKE=2; 
-randomDieMove=$((RANDOM%6+1))
-Option=$((RANDOM%3))
+
+function moveCondition(){
+
+	if [ $1 -le 0 ]
+	then
+		echo $1=$INITIAL_POSITION;
+	elif [ $1 -ge $WINNING_POSITION ]
+	then
+		echo win;
+	fi 
+}
+
+while [ $firstPlayerPosition -le $WINNING_POSITION ]
+do
+	randomDieMove=$((RANDOM%6+1))
+	Option=$((RANDOM%3))
 
 	case $Option in
 	$NOPLAY)
@@ -19,3 +34,6 @@ Option=$((RANDOM%3))
 	$SNAKE)
 		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ));;
 	esac
+	 moveCondition $firstPlayerPosition
+
+done
